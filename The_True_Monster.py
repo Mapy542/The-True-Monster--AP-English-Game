84dab40f8c586153f2e34db8c1139a6abdebdaf3
Eli <elimbukoski@gmail.com>
2022-12-15 13:55:01 -0500

    finish project

--- The_True_Monster.py
+++ The_True_Monster.py
@@ -12,22 +12,24 @@ from tkinter import font
 from textwrap import wrap
 
 
-class Character:
+class Character:  # class to represent a character within the story
     def __init__(self, health=10, enthusiasm=5, education=0, ability=5, name="Character"):
+        # luck is a random number between 1 and 5 no matter what
         self.luck = random.randint(1, 5)
-        self.health = health
-        self.enthusiasm = enthusiasm
-        self.education = education
-        self.ability = ability
-        self.name = name
-        self.sick = False
+        self.health = health  # unused in this game
+        self.enthusiasm = enthusiasm  # represents how much the character wants to do the task
+        self.education = education  # represents how much the character knows about the task
+        self.ability = ability  # represents how good the character is at the task
+        self.name = name  # name of the character
+        self.sick = False  # whether or not the character is sick
 
+        # if the character has good luck, they get a bonus
         if (self.luck == 5 and random.randint(1, 2) == 1):
             self.ability += random.randint(1, 3)
             self.enthusiasm += random.randint(1, 3)
             self.education += random.randint(1, 3)
 
-    def Make_Random(self):
+    def Make_Random(self):  # randomizes character after initialization or whenever called
         self.luck = random.randint(1, 5)
         self.health = random.randint(1, 10)
         self.enthusiasm = random.randint(1, 5)
@@ -39,32 +41,36 @@ class Character:
             self.enthusiasm += random.randint(1, 3)
             self.education += random.randint(1, 3)
 
-    def Luck_Check(self):
+    def Luck_Check(self):  # checks if the character has good luck and gives a bonus if they do
         if (self.luck == 5 and random.randint(1, 2) == 1):
             self.ability += random.randint(1, 3)
             self.enthusiasm += random.randint(1, 3)
             self.education += random.randint(1, 3)
 
 
-def Display_Dialogue(text):
+def Display_Dialogue(text):  # converts strings into a display in the GUI
+    # splits the text into a list of strings as listbox can't handle new lines
     text = text.split("\n")
     display_text = []
-    for section in text:
-        if len(section) > 120:
-            section = wrap(section, 120)
+    for section in text:  # for each line of text
+        if len(section) > 120:  # if the line is too long
+            section = wrap(section, 120)  # wrap the line to fit the screen
             for i in range(len(section)):
                 if i < len(section)-1:
+                    # add a line break to the end of each line except the last if wanted
                     section[i] = section[i] + ""
+                # add the line to the display text
                 display_text.append(section[i])
         else:
+            # if the line is short enough, add it to the display text
             display_text.append(section)
 
-    dialogue.clear()
-    for i in range(len(display_text)):
+    dialogue.clear()  # clear the dialogue box
+    for i in range(len(display_text)):  # add the text to the dialogue box
         dialogue.append(display_text[i])
 
 
-def Display_Options(options_inputs):
+def Display_Options(options_inputs):  # converts strings into a display in the GUI
     displaytext = []
     for section in options_inputs:
         if len(section) > 40:
@@ -76,13 +82,12 @@ def Display_Options(options_inputs):
         else:
             displaytext.append(section)
 
-    print(displaytext)
     options.clear()
     for i in range(len(displaytext)):
         options.append(displaytext[i])
 
 
-def Display_Characters(objects):
+def Display_Characters(objects):  # converts strings into a display in the GUI
     displaytext = []
     for person in objects:
         displaytext.append(person.name)
@@ -97,19 +102,21 @@ def Display_Characters(objects):
         characters.append(displaytext[i])
 
 
-def TextFile(num):
+def TextFile(num):  # reads the text file and returns the text for the given scene number
     try:
-        with open('Dialouge.txt', "r") as file:
-            text = file.read()
-            text = text.split('*')
+        with open('Dialouge.txt', "r") as file:  # opens the text file
+            text = file.read()  # reads the text file
+            text = text.split('*')  # splits the text file into scenes
+            # removes empty strings from the list
             text = [x for x in text if x]
-            for i in range(len(text)):
+            # ^ this kind of pythonic code is called a list comprehension and is amazing
+            for i in range(len(text)):  # for each scene
+                # split the scene into lines into a new variable
                 temp = text[i].split('\n')
-                print(temp)
-                numdial = temp[0]
-                if str(num) == numdial:
+                numdial = temp[0]  # find the scene number
+                if str(num) == numdial:  # if the scene number matches the one we want
+                    # get the text for the scene minus the scene number and first line break
                     val = text[i][(len(numdial)+1):]
-                    print(val)
                     return val
 
     except FileNotFoundError:
@@ -117,22 +124,25 @@ def TextFile(num):
         exit()
 
 
-def IsAlive(Living, name):
+# list of characters attribute checkers
+def IsAlive(Living, name):  # checks if a character is alive
     for person in Living:
         if person.name == name:
             return True
     return False
 
 
-def GetBadLuck(Living):
+def GetBadLuck(Living):  # gets a list of characters with bad luck
     peeps = []
     for person in Living:
+        # if the character has a lower .luck they have a higher chance of getting bad luck
         if random.randint(0, 5) - person.luck > 0:
             peeps.append(person.name)
-    return peeps
+    return peeps  # returns a list of characters with bad luck
+    # list is helpful for when multiple characters can get bad luck
 
 
-def GetGoodLuck(Living):
+def GetGoodLuck(Living):  # gets a list of characters with good luck
     peeps = []
     for person in Living:
         if random.randint(0, 5) + person.luck > 5:
@@ -140,28 +150,28 @@ def GetGoodLuck(Living):
     return peeps
 
 
-def Kill(Living, name):
+def Kill(Living, name):  # kills a character by finding them in the list and removing them
     for person in Living:
         if person.name == name:
             Living.remove(person)
     return Living
 
 
-def MarkSick(Living, name):
+def MarkSick(Living, name):  # marks a character as sick
     for person in Living:
         if person.name == name:
             person.sick = True
     return Living
 
 
-def IsSick(Living, name):
+def IsSick(Living, name):  # checks if a character is sick
     for person in Living:
         if person.name == name:
             return person.sick
     return False
 
 
-def SicknessCheck(Living):
+def SicknessCheck(Living):  # checks if a character is sick and kills them if they have bad luck
     for person in Living:
         if person.sick == True:
             person.sick = False
@@ -172,7 +182,7 @@ def SicknessCheck(Living):
     return Living, None
 
 
-def continuegame():
+def continuegame():  # advance game based on parameters
     global Level, Living, options
     if Level == 0:
         if "Creator" in options.value:
@@ -245,6 +255,7 @@ def continuegame():
             user_options = ['Next']
             text = TextFile(7)
             Living.append(Character(name='Henry'))
+            Living[len(Living)-1].Make_Random()
         if 'Stay with your family' in options.value:
             Level = 9
             user_options = ['Next']
@@ -546,7 +557,7 @@ def continuegame():
     elif Level == 50:
         Level = 55
         text = TextFile(55)
-        user_options = ['Next']
+        user_options = ['Sneak into the engine room', 'Make spark generator']
     elif Level == 51:
         Level = 52
         text = TextFile(52)
@@ -649,7 +660,7 @@ def continuegame():
         text = TextFile(65)
         user_options = ['Next']
     elif Level == 65:
-        if 'Henry' in IsAlive(Living):
+        if IsAlive(Living, 'Henry'):
             Level = 67
             text = TextFile(67)
             user_options = ['Next']
@@ -669,41 +680,255 @@ def continuegame():
         Level = 68
         text = TextFile(68)
         user_options = ['Next']
+    elif Level == 68:
+        Level = 69
+        text = TextFile(69)
+        user_options = ['Next']
+    elif Level == 69:
+        if 'Elizabeth' in IsAlive(Living):
+            Level = 71
+            text = TextFile(71)
+            user_options = ['Go after the monster', 'Let it go.']
+        else:
+            Level = 70
+            text = TextFile(70)
+            user_options = ['Go after the monster', 'Let it go.']
+    elif Level == 70:
+        if 'Let it go.' in options.value:
+            Level = 72
+            text = TextFile(72)
+            user_options = ['Restart', 'Quit']
+        if 'Go after the monster' in options.value:
+            Level = 73
+            text = TextFile(73)
+            user_options = ['Next']
+    elif Level == 71:
+        if 'Let it go.' in options.value:
+            Level = 72
+            text = TextFile(72)
+            user_options = ['Restart', 'Quit']
+        if 'Go after the monster' in options.value:
+            Level = 73
+            text = TextFile(73)
+            user_options = ['Next']
+    elif Level == 72:
+        if 'Restart' in options.value:
+            Level = 0
+            text = 'You have restarted the game. \n Use the character creator or randomize your stats.'
+            user_options = ['Creator', 'Random']
+            Living = []
+        if 'Quit' in options.value:
+            exit()
+    elif Level == 73:
+        Level = 74
+        text = TextFile(74)
+        user_options = ['Next']
+    elif Level == 74:
+        if 'Victor' in GetGoodLuck(Living):
+            Level = 76
+            text = TextFile(76)
+            user_options = ['Next']
+        else:
+            Level = 75
+            text = TextFile(75)
+            user_options = ['Quit', 'Restart']
+    elif Level == 75:
+        if 'Restart' in options.value:
+            Level = 0
+            text = 'You have restarted the game. \n Use the character creator or randomize your stats.'
+            user_options = ['Creator', 'Random']
+            Living = []
+        if 'Quit' in options.value:
+            exit()
+    elif Level == 76:
+        Level = 77
+        text = TextFile(77)
+        user_options = ['Next']
+    elif Level == 77:
+        if Living[0].enthusiasm < 4:
+            Level = 78
+            text = TextFile(78)
+            user_options = ['Restart', 'Quit']
+        else:
+            Level = 79
+            text = TextFile(79)
+            user_options = ['Kill the monster', 'Help him']
+    elif Level == 78:
+        if 'Restart' in options.value:
+            Level = 0
+            text = 'You have restarted the game. \n Use the character creator or randomize your stats.'
+            user_options = ['Creator', 'Random']
+            Living = []
+        if 'Quit' in options.value:
+            exit()
+    elif Level == 79:
+        if 'Kill the monster' in options.value:
+            if 'Victor' in GetGoodLuck(Living):
+                Level = 81
+                text = TextFile(81)
+                user_options = ['Restart', 'Quit']
+            else:
+                Level = 80
+                text = TextFile(80)
+                user_options = ['Restart', 'Quit']
+        if 'Help him' in options.value:
+            Level = 82
+            text = TextFile(82)
+            user_options = ['Next']
+    elif Level == 80:
+        if 'Restart' in options.value:
+            Level = 0
+            text = 'You have restarted the game. \n Use the character creator or randomize your stats.'
+            user_options = ['Creator', 'Random']
+            Living = []
+        if 'Quit' in options.value:
+            exit()
+    elif Level == 81:
+        if 'Restart' in options.value:
+            Level = 0
+            text = 'You have restarted the game. \n Use the character creator or randomize your stats.'
+            user_options = ['Creator', 'Random']
+            Living = []
+        if 'Quit' in options.value:
+            exit()
+    elif Level == 82:
+        Level = 83
+        text = TextFile(83)
+        user_options = ['Avoid the monsters request', 'Keep your promise']
+    elif Level == 83:
+        if 'Keep your promise' in options.value:
+            Level = 85
+            text = TextFile(85)
+            user_options = ['Next']
+        if 'Avoid the monsters request' in options.value:
+            Level = 84
+            text = TextFile(84)
+            user_options = ['Restart', 'Quit']
+    elif Level == 84:
+        if 'Restart' in options.value:
+            Level = 0
+            text = 'You have restarted the game. \n Use the character creator or randomize your stats.'
+            user_options = ['Creator', 'Random']
+            Living = []
+        if 'Quit' in options.value:
+            exit()
+    elif Level == 85:
+        Level = 86
+        text = TextFile(86)
+        user_options = ['Next']
+    elif Level == 86:
+        Level = 87
+        text = TextFile(87)
+        user_options = ['Finish the second creature', 'Stop your work']
+    elif Level == 87:
+        if 'Finish the second creature' in options.value:
+            Level = 88
+            text = TextFile(88)
+            user_options = ['Next']
+        if 'Stop your work' in options.value:
+            Level = 91
+            text = TextFile(91)
+            user_options = ['Next']
+    elif Level == 88:
+        if 'Victor' in GetGoodLuck(Living):
+            Level = 90
+            text = TextFile(90)
+            user_options = ['Restart', 'Quit']
+        else:
+            Level = 89
+            text = TextFile(89)
+            user_options = ['Restart', 'Quit']
+    elif Level == 89 or Level == 90:
+        if 'Restart' in options.value:
+            Level = 0
+            text = 'You have restarted the game. \n Use the character creator or randomize your stats.'
+            user_options = ['Creator', 'Random']
+            Living = []
+        if 'Quit' in options.value:
+            exit()
+    elif Level == 91:
+        if not IsAlive(Living, 'Elizabeth'):
+            Level = 92
+            text = TextFile(92)
+            user_options = ['Restart', 'Quit']
+        else:
+            Level = 93
+            text = TextFile(93)
+            user_options = ['Next']
+    elif Level == 92:
+        if 'Restart' in options.value:
+            Level = 0
+            text = 'You have restarted the game. \n Use the character creator or randomize your stats.'
+            user_options = ['Creator', 'Random']
+            Living = []
+        if 'Quit' in options.value:
+            exit()
+    elif Level == 93:
+        Level = 94
+        text = TextFile(94)
+        user_options = ['Next']
+    elif Level == 94:
+        Level = 95
+        text = TextFile(95)
+        user_options = ['Next']
+    elif Level == 95:
+        Level = 96
+        text = TextFile(96)
+        user_options = ['Next']
+    elif Level == 96:
+        Level = 97
+        text = TextFile(97)
+        user_options = ['Restart', 'Quit']
+    elif Level == 97:
+        if 'Restart' in options.value:
+            Level = 0
+            text = 'You have restarted the game. \n Use the character creator or randomize your stats.'
+            user_options = ['Creator', 'Random']
+            Living = []
+        if 'Quit' in options.value:
+            exit()
 
-    Display_Dialogue(text)
+    Display_Dialogue(text)  # display the text for the next game level
+    # display the options for the next game level
     Display_Options(user_options)
+    # display the characters for the next game level
     Display_Characters(Living)
 
 
-#-----------------#
-app = App(title="The True Monster", width=1200, height=600, layout="grid")
+# GUI object creation
+app = App(title="The True Monster", width=1200,
+          height=600, layout="grid")  # main window
 dialogue = ListBox(app, grid=[0, 0, 8, 6], scrollbar=True, items=[
-                   'dialogue'], width=900, height=250)
+                   'dialogue'], width=900, height=250)  # main multi-select box that I am using to display the text
+# make it a better font by modifying the tkinter sub object
 dialogue.font = "Courier"
-title = Text(app, text="The True", grid=[9, 0, 1, 1], size=30)
-title.font = 'Lucida Bright'
+title = Text(app, text="The True", grid=[
+             9, 0, 1, 1], size=30)  # show the title
+title.font = 'Lucida Handwriting'
 title2 = Text(app, text='Monster', grid=[9, 1, 1, 2], size=30)
-title2.font = 'Lucida Bright'
+title2.font = 'Lucida Handwriting'
 options_text = Text(app, grid=[3, 7, 8, 1], text="Options")
 options = ListBox(app, grid=[3, 8, 2, 3], scrollbar=True, items=[
                   'options'], width=450, height=300, multiselect=True)
-options.font = "Courier"
+options.font = "Times New Roman"
 characters_text = Text(app, grid=[0, 7, 2, 1], text="Characters")
 characters = ListBox(app, grid=[0, 8, 2, 3],
                      scrollbar=True, items=['characters'], width=450, height=300)
+# use a monospaced font to make it easier to read as the lines are all the same length
 characters.font = "Courier"
 
 submit = PushButton(app, grid=[9, 8, 2, 1],
-                    text="Submit", command=continuegame)
+                    text="Submit", command=continuegame)  # submit button that advances the game
 
-# Enter Character Creator?
-Level = 0
-Living = []
+# Enter Character Creator or Randomize by default
+Level = 0  # global variable to keep track of the level as the game advances
+Living = []  # global variable to keep track of the characters as they are created
+# Use character class
 
 Display_Dialogue(
-    "Before we begin, would you like to enter the character creator, or would you like random characters?")
+    "Before we begin, would you like to enter the character creator, or would you like random characters? \n Select one of the options below. Then click submit.  Selecting 'Next' is unnecessary when there are no other options.")
 
 Display_Options(["Creator", "Random"])
 Display_Characters(Living)
 
-app.display()
+app.display()  # display the GUI with loop so the UI is responsive
